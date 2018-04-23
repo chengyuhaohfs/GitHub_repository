5c5072e8acc56658d663ad710842ffed6887828d
chengyuhao <chengyu1184789@163.com>
2018-04-23 22:40:44 +0800

    增加一个生成指定空路径的操作
    CheckAndCreateDir

--- use_mfc/CMyFile.cpp
+++ use_mfc/CMyFile.cpp
@@ -13,7 +13,62 @@ CMyFile::~CMyFile()
 
 BOOL CMyFile::CheckAndCreateDir(CString csNewDir)
 {
+	CString csMsg = L"";
+	// 先尝试直接生成目录
+	if (!CreateDirectory(csNewDir + L"\\", NULL))
+	{
+		// 当创建失败时要尝试删除文件夹
+		if (!RemoveDirectory(csNewDir))
+		{
+			// 当删除文件夹失败时，要尝试清空文件夹后再删除
+			CString csFindFile = L"";
+			HANDLE hfirstFile = NULL;
+			WIN32_FIND_DATAW wFildAtt;
 
+			hfirstFile = FindFirstFile(csNewDir + L"\\*", &wFildAtt);
+			if (!hfirstFile)
+			{
+				csMsg.Format(L"获取首个文件失败！", csMsg);
+				AfxMessageBox(csMsg);
+				return FALSE;
+			}
+			else if (FindNextFile(hfirstFile, &wFildAtt))
+			{
+				while (FindNextFile(hfirstFile, &wFildAtt))
+				{
+					csMsg.Format(L"%s\\%s", csNewDir, wFildAtt.cFileName);
+					if (!DeleteFile(csMsg))
+					{
+						csMsg.Format(L"删除文件 %s 失败！", csMsg);
+						AfxMessageBox(csMsg);
+						return FALSE;
+					}
+				}
+			}
+			csMsg.Format(L"生成路径 %s 成功！", csNewDir);
+			AfxMessageBox(csMsg);
+			return TRUE;
+		}
+		// 如果依然失败，要考虑目录是否可用
+		if (!CreateDirectory(csNewDir + L"\\", NULL))
+		{
+			csMsg.Format(L"生成路径 %s 失败！", csNewDir);
+			AfxMessageBox(csMsg);
+			return FALSE;
+		}
+		else
+		{
+			csMsg.Format(L"生成路径 %s 成功！", csNewDir);
+			AfxMessageBox(csMsg);
+			return TRUE;
+		}
+	}
+	else
+	{
+		csMsg.Format(L"生成路径 %s 成功！", csNewDir);
+		AfxMessageBox(csMsg);
+		return TRUE;
+	}
 
 	return TRUE;
 }
--- use_mfc/useMfc.cpp
+++ use_mfc/useMfc.cpp
@@ -5,10 +5,37 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+	// 打印“hello world”
 	CString csMsg = L"Hello world from MFC";
-	AfxMessageBox(csMsg);
+/*	AfxMessageBox(csMsg);*/
+
+	// 获取当前路径
+	LPWSTR wCurrentDir = csMsg.GetBuffer(MAX_PATH);
+	csMsg.ReleaseBuffer();
+
+	GetCurrentDirectory(MAX_PATH, wCurrentDir);
+/*	AfxMessageBox(wCurrentDir);*/
+
+	// 生成指定路径
+	CMyFile imyfile;
+	CString csNewDir = L"";
+	csNewDir.Format(L"%s\\testDir", wCurrentDir);
+
+	if (!imyfile.CheckAndCreateDir(csNewDir))
+	{
+		return 0;
+	}
+	
+/*
+	if (!RemoveDirectory(csNewDir))
+	{
+		csMsg.Format(L"删除路径 %s 失败！", csNewDir);
+		AfxMessageBox(csMsg);
+		return 0;
+	}*/
 
 	// I will split the sentence
+/*
 	CMyString imystr;
 	CStringArray csArray;
 	CString cstr = L"hu, fu, sheng";
@@ -17,9 +44,9 @@ int main(int argc, char** argv)
 	imystr.SplitStr(cstr, L",", csArray);
 
 	CString csFind = L"";
-	imystr.FindStr(cstr, L"hu,", L",", csFind);
+	imystr.FindStr(cstr, L"hu,", L",", csFind);*/
 
-	cout << "hello world" << endl;
+/*	cout << "hello world" << endl;*/
 
 	return 1;
 }
