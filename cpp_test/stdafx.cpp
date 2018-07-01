defd01e58e90099f6f36f2aa538a81c5eb3d6d8f
chengyuhao <chengyu1184789@163.com>
2018-07-01 21:05:01 +0800

    更新 cpp_test
    增加一段创建管道并生成新的进程的代码

--- cpp_test/stdafx.cpp
+++ cpp_test/stdafx.cpp
@@ -4,6 +4,8 @@
 #include "memory"
 #include "stdafx.h"
 #include "vector"
+#include "afxwin.h"
+#include "comutil.h"
 
 typedef std::valarray<double> ArrayDb;
 
@@ -53,25 +55,108 @@ public:
 
 int main(int argc, char** argv) {
 	using namespace std;
-	Student hfs("hfs");
-	valarray<double> scores(5.0, 5);
-	scores.resize(6, 9.8);
-	scores[5] = 9.9;
-	scores.shift(3);
-/*	cout << "the size of valarray is " << scores.size() << endl;*/
-	for(int i = 0; i < (int)scores.size(); i++){
-		cout << scores[i] << ", ";
-	}
-	cout << endl;
-	cout << hfs.sum() << endl;
+	
+	CString csMsg = L"";
+	size_t const WORD_SIZE = 2048;
 
-	cout << pow(2, 3) << endl;
+	HANDLE hRead, hWrite;
+	SECURITY_ATTRIBUTES sa;
+	sa.nLength = sizeof(sa);
+	sa.bInheritHandle = TRUE;
+	sa.lpSecurityDescriptor = NULL;
 
-	vector<string> str{ "010-12345678", "0319-9876543", "021-123456789" };
-	for (string st : str)
+	if (!CreatePipe(&hRead, &hWrite, &sa, WORD_SIZE - 1))
 	{
-		cout << st << endl;
+		csMsg.Format(L"创建输出管道失败！");
+		AfxMessageBox(csMsg);
+
+		CloseHandle(hRead);
+		CloseHandle(hWrite);
+		return 0;
+	}
+	
+	STARTUPINFO st;
+	GetStartupInfo(&st);
+	st.cb = sizeof(st);
+	st.dwFlags = STARTF_USESTDHANDLES | STARTF_USESHOWWINDOW;
+	st.wShowWindow = SW_HIDE;
+
+	st.hStdError = hWrite;
+	st.hStdOutput = hWrite;
+/*	st.hStdInput = GetStdHandle(STD_INPUT_HANDLE);*/
+
+	PROCESS_INFORMATION pi;
+
+	WCHAR wcApp[] = L"";
+	WCHAR wcCmd[] = L"tasklist";
+
+/*	WCHAR* lpApp = wcApp;*/
+	WCHAR* lpApp = NULL;
+	WCHAR* lpCmd = wcCmd;
+
+	if (!CreateProcess(lpApp, lpCmd, NULL, NULL, TRUE, 0, NULL, NULL, &st, &pi))
+	{
+		csMsg.Format(L"创建进程失败！");
+		AfxMessageBox(csMsg);
+
+		CloseHandle(hRead);
+		CloseHandle(hWrite);
+		return 0;
 	}
 
+	WaitForSingleObject(pi.hProcess, 500);
+
+	char chBuffer[WORD_SIZE] = "";
+	memset(chBuffer, 0, WORD_SIZE);
+
+	DWORD exitCode = 0;
+	GetExitCodeProcess(pi.hProcess, &exitCode);
+
+	DWORD nByteRead = 0, nByteAvail = 0, nByteLeft = 0;
+
+	while (true)
+	{
+		if (!PeekNamedPipe(hRead, chBuffer, WORD_SIZE - 1, &nByteRead, &nByteAvail, &nByteLeft))
+		{
+			csMsg.Format(L"预读管道失败！");
+			AfxMessageBox(csMsg);
+
+			CloseHandle(hRead);
+			CloseHandle(hWrite);
+			return 0;
+		}
+
+		GetExitCodeProcess(pi.hProcess, &exitCode);
+
+		if (0 == nByteRead)
+		{
+			if (STILL_ACTIVE != exitCode)
+			{
+				break;
+			}
+
+			continue;
+		}
+
+		memset(chBuffer, 0, WORD_SIZE);
+		if (!ReadFile(hRead, chBuffer, WORD_SIZE - 1, &nByteRead, NULL))
+		{
+			csMsg.Format(L"读管道失败！");
+			AfxMessageBox(csMsg);
+
+			CloseHandle(hRead);
+			CloseHandle(hWrite);
+			return 0;
+		}
+
+		cout << chBuffer;
+		memset(chBuffer, 0, WORD_SIZE);
+	}
+
+	
+
+	CloseHandle(hRead);
+	CloseHandle(hWrite);
 	return 0;
 }
+
