0919d40e3e4561644fb3287171c543d0d2c7142c
chengyuhao <chengyu1184789@163.com>
2018-07-01 21:03:34 +0800

    增加一个winConsole工程
    创建一个可以执行powershell指令并收集打印信息的窗口程序

---
+++ winConsole/winConsole.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace winConsole
+{
+    partial class winConsole
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_cmd = new System.Windows.Forms.TextBox();
+            this.richTextBox_output = new System.Windows.Forms.RichTextBox();
+            this.button_ok = new System.Windows.Forms.Button();
+            this.button_cancel = new System.Windows.Forms.Button();
+            this.button_show_log = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 484);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Command";
+            // 
+            // textBox_cmd
+            // 
+            this.textBox_cmd.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox_cmd.Location = new System.Drawing.Point(81, 480);
+            this.textBox_cmd.Name = "textBox_cmd";
+            this.textBox_cmd.Size = new System.Drawing.Size(779, 23);
+            this.textBox_cmd.TabIndex = 1;
+            this.textBox_cmd.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_keypress);
+            // 
+            // richTextBox_output
+            // 
+            this.richTextBox_output.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox_output.Font = new System.Drawing.Font("Consolas", 8.765218F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox_output.Location = new System.Drawing.Point(15, 12);
+            this.richTextBox_output.Name = "richTextBox_output";
+            this.richTextBox_output.Size = new System.Drawing.Size(845, 448);
+            this.richTextBox_output.TabIndex = 2;
+            this.richTextBox_output.Text = "";
+            // 
+            // button_ok
+            // 
+            this.button_ok.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_ok.Location = new System.Drawing.Point(614, 524);
+            this.button_ok.Name = "button_ok";
+            this.button_ok.Size = new System.Drawing.Size(75, 23);
+            this.button_ok.TabIndex = 3;
+            this.button_ok.Text = "Ok";
+            this.button_ok.UseVisualStyleBackColor = true;
+            this.button_ok.Click += new System.EventHandler(this.button_ok_Click);
+            // 
+            // button_cancel
+            // 
+            this.button_cancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button_cancel.Location = new System.Drawing.Point(785, 524);
+            this.button_cancel.Name = "button_cancel";
+            this.button_cancel.Size = new System.Drawing.Size(75, 23);
+            this.button_cancel.TabIndex = 3;
+            this.button_cancel.Text = "Cancel";
+            this.button_cancel.UseVisualStyleBackColor = true;
+            this.button_cancel.Click += new System.EventHandler(this.button_cancel_Click);
+            // 
+            // button_show_log
+            // 
+            this.button_show_log.Location = new System.Drawing.Point(81, 524);
+            this.button_show_log.Name = "button_show_log";
+            this.button_show_log.Size = new System.Drawing.Size(82, 23);
+            this.button_show_log.TabIndex = 4;
+            this.button_show_log.Text = "Show Log";
+            this.button_show_log.UseVisualStyleBackColor = true;
+            this.button_show_log.Click += new System.EventHandler(this.button_show_log_Click);
+            // 
+            // winConsole
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(872, 559);
+            this.Controls.Add(this.button_show_log);
+            this.Controls.Add(this.button_cancel);
+            this.Controls.Add(this.button_ok);
+            this.Controls.Add(this.richTextBox_output);
+            this.Controls.Add(this.textBox_cmd);
+            this.Controls.Add(this.label1);
+            this.Name = "winConsole";
+            this.Text = "Self Command";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.form_closing);
+            this.Load += new System.EventHandler(this.winConsole_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_cmd;
+        private System.Windows.Forms.RichTextBox richTextBox_output;
+        private System.Windows.Forms.Button button_ok;
+        private System.Windows.Forms.Button button_cancel;
+        private System.Windows.Forms.Button button_show_log;
+    }
+}
+
---
+++ winConsole/winConsole.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace winConsole
+{
+    public partial class winConsole : Form
+    {
+        string cmd = string.Empty;
+        string readData = string.Empty;
+
+/*        Thread thread;*/
+
+        Process pro;
+        Process showlog;
+
+        StreamReader output;
+        StreamWriter input;
+
+        // 建一个文件流用于保存日志
+        static string filename = "command_log.txt";
+        FileStream fs;
+        StreamWriter sw;
+        public winConsole()
+        {
+            InitializeComponent();
+        }
+
+        private void button_cancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBox_keypress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == 13)
+            {
+                // 发送前先判断链接是否还在
+                if(pro.HasExited)
+                {
+                    MessageBox.Show("进程状态判断不正确，弹框错误！");
+                }
+                button_ok.Focus();
+                SendKeys.Send("{Enter}");
+            }
+        }
+
+        private void button_ok_Click(object sender, EventArgs e)
+        {
+            cmd = textBox_cmd.Text;
+            if((cmd == "cls") || (cmd == "clear"))
+            {
+                fs = new FileStream(filename, FileMode.Append | FileMode.Create);
+                sw = new StreamWriter(fs);
+                sw.AutoFlush = true;
+                sw.Write(richTextBox_output.Text);
+                sw.Close();
+                fs.Close();
+
+                richTextBox_output.Text = string.Empty;
+                input.Write("\n");
+            }
+            else
+            {
+                input.WriteLine(@cmd);
+                input.Write("\n");
+            }
+            textBox_cmd.Focus();
+            textBox_cmd.SelectAll();
+        }
+
+        private void getout(object sender, DataReceivedEventArgs e)
+        {
+            readData = e.Data;
+            if (!string.IsNullOrEmpty(readData))
+            {
+/*                richTextBox_output.Text += readData + "\n";*/
+                richTextBox_output.AppendText(readData + "\n");
+            }
+            else
+            {
+                richTextBox_output.ScrollToCaret();
+            }
+        }
+
+        private void temp()
+        {
+            if (pro.Start())
+            {
+                string outputtext = string.Empty;
+
+                input = pro.StandardInput;
+                input.WriteLine(@cmd);
+                input.Dispose();
+
+                output = pro.StandardOutput;
+                outputtext = output.ReadToEnd();
+                output.Dispose();
+
+                input.Close();
+                output.Close();
+                
+                if (!pro.WaitForExit(3000))
+                {
+                    MessageBox.Show("Command execute failed!");
+                    return;
+                }
+
+                richTextBox_output.Text += outputtext + "\n";
+
+
+
+                fs = new FileStream(filename, FileMode.Append | FileMode.Create);
+                sw = new StreamWriter(fs);
+                sw.AutoFlush = true;
+                sw.Write(outputtext + "\n");
+                sw.Close();
+                fs.Close();
+            }
+            else
+            {
+                MessageBox.Show("运行程序失败！");
+            }
+        }
+
+        private void winConsole_Load(object sender, EventArgs e)
+        {
+            // 根据当前时间获取一个文件名
+            string date = DateTime.Now.ToShortDateString();
+            filename = date.Replace('/', '_') + "-log.txt";
+
+            showlog = new Process();
+
+            showlog.StartInfo.FileName               = "E:\\Program Files (x86)\\Notepad++\\notepad++.exe";
+            showlog.StartInfo.Arguments              = filename;
+            showlog.StartInfo.WindowStyle            = ProcessWindowStyle.Hidden;
+            showlog.StartInfo.CreateNoWindow         = true;
+            showlog.StartInfo.UseShellExecute        = false;
+            showlog.StartInfo.RedirectStandardInput  = true;
+            showlog.StartInfo.RedirectStandardOutput = true;
+            
+            pro = new Process();
+
+            pro.StartInfo.FileName = "powershell.exe"; //cmd.exe
+            pro.StartInfo.UseShellExecute = false;
+            pro.StartInfo.RedirectStandardOutput = true;
+            pro.StartInfo.RedirectStandardInput = true;
+            pro.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            pro.StartInfo.CreateNoWindow = true;
+
+            richTextBox_output.Text = string.Empty;
+
+            pro.Start();
+
+            input = pro.StandardInput;
+
+            pro.OutputDataReceived += new DataReceivedEventHandler(getout);
+            pro.BeginOutputReadLine();
+
+            // 测试按键自动处理程序
+//             thread = new Thread(new ThreadStart(threadRun));
+//             thread.Start();
+
+            Thread.Sleep(500);
+            SendKeys.Send("{Enter}");
+
+            this.Activate();
+        }
+
+        private void threadRun()
+        {
+            MessageBox.Show("test auto close", "tips", MessageBoxButtons.YesNo);
+        }
+
+        private void button_show_log_Click(object sender, EventArgs e)
+        {
+            if(!showlog.Start())
+            {
+                MessageBox.Show("打开日志文件失败！");
+            }
+        }
+
+        private void form_closing(object sender, FormClosingEventArgs e)
+        {
+            fs = new FileStream(filename, FileMode.Append | FileMode.Create);
+            sw = new StreamWriter(fs);
+            sw.AutoFlush = true;
+            sw.Write(richTextBox_output.Text);
+            sw.Close();
+            fs.Close();
+
+            pro.Close();
+
+//             thread.Abort();
+//             thread.Join();
+        }
+    }
+}
